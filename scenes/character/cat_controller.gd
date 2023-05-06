25dafbfca15c41ac502622f95e9f1ed2c403d29c
Kinga <niu.ula.a@gmail.com>
2023-05-06 18:54:41 +0200

    another remake - lets use animated sprite 2d in the end

--- scenes/character/cat_controller.gd
+++ scenes/character/cat_controller.gd
@@ -1,4 +1,24 @@
-extends CharacterBody2D
+extends AnimatedSprite2D
+
+@onready var anim_tree = $AnimationTree
 
 
+func _process(_delta):
+	if Input.is_action_just_released("up"):
+		jump()
+	
+	if Input.is_action_just_released("right"):
+		walk()
 
+	if !is_playing():
+		play("idle")
+
+
+func jump():
+	play("jump")
+
+func idle():
+	play("idle")
+
+func walk():
+	play("walk")
