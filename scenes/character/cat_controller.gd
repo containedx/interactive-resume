09bda3f3148a2c0b4f5cc63c5428c5f0dc82661e
Kinga <niu.ula.a@gmail.com>
2023-05-06 19:30:36 +0200

    simple movement

--- scenes/character/cat_controller.gd
+++ scenes/character/cat_controller.gd
@@ -1,24 +1,26 @@
 extends AnimatedSprite2D
 
+@onready var environment = $"../environment"
 
 func _process(_delta):
 	handle_movement()
 
 
 func handle_movement():
-	if Input.is_action_just_released("up"):
+	if Input.is_action_just_pressed("up"):
 		if animation == "lie":
 			idle()
 		else:
 			jump()
-	if Input.is_action_just_released("right"):
+	if Input.is_action_just_pressed("right"):
 		flip_h = false
 		walk()
-	if Input.is_action_just_released("left"):
+	if Input.is_action_just_pressed("left"):
 		flip_h = true
 		walk()
-	if Input.is_action_just_released("down"):
+	if Input.is_action_just_pressed("down"):
 		lie()
+		environment.stop()
 
 	if !is_playing():
 		idle()
@@ -27,14 +29,14 @@ func handle_movement():
 func jump():
 	play("jump")
 
-
 func idle():
 	play("idle")
-
 
 func walk():
 	play("walk")
 
-
 func lie():
 	play("lie")
+
+func run():
+	play("run")
---
+++ scenes/environment.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+@onready var cat = $"../cat"
+
+var target_position : Vector2 = position
+var moving : bool = false
+
+func _process(delta):
+	handle_input()
+	handle_movement(delta)
+	
+
+func handle_movement(delta):
+	if moving:
+		position = position.lerp(target_position, delta)
+		
+		if position.distance_squared_to(target_position) > 100000:
+			cat.run()
+		else :
+			cat.walk()
+		
+		if position.distance_squared_to(target_position) <= 5:
+			moving= false
+			cat.idle()
+
+func handle_input():
+	if Input.is_action_pressed("right"):
+		target_position.x -= 10
+		moving = true
+	if Input.is_action_pressed("left"):
+		target_position.x += 10
+		moving = true
+
+func stop():
+	moving = false
+	target_position = position
