6a0fd4df32f7c80d7ea64c2e70e79135d4de777d
Kinga <niu.ula.a@gmail.com>
2023-05-07 13:47:43 +0200

    pause

--- scenes/game_controller.gd
+++ scenes/game_controller.gd
@@ -3,8 +3,11 @@ extends Node2D
 @onready var anim_player = $AnimationPlayer
 @onready var content = $environment/content
 
+var game_started : bool = false
+
 func _ready():
 	content.visible = false
 
 func start():
 	anim_player.play("start")
+	game_started = true
--- scenes/menus/menu.gd
+++ scenes/menus/menu.gd
@@ -2,7 +2,20 @@ extends Control
 
 @onready var game_controller = get_tree().get_current_scene()
 
+func _ready():
+	pause_game(true)
+
+func _input(_event):
+	if Input.is_action_just_pressed("pause") && game_controller.game_started:
+		$button_menu/start.visible = false
+		visible = !visible
+		pause_game(visible)
 
 
 func _on_start_button_down():
+	pause_game(false)
 	game_controller.start()
+
+
+func pause_game( value : bool):
+	get_tree().paused = value
