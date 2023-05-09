2b048f9de9617b3cb29efd5efee74eea9a151226
Kinga <niu.ula.a@gmail.com>
2023-05-09 17:01:56 +0200

    credits

--- scenes/menus/menu.gd
+++ scenes/menus/menu.gd
@@ -3,11 +3,14 @@ extends Control
 @onready var game_controller = get_tree().get_current_scene()
 
 func _ready():
+	$button_menu/start_again.visible = false
+	$credits.visible = false
 	pause_game(true)
 
 func _input(_event):
 	if Input.is_action_just_pressed("pause") && game_controller.game_started:
 		$button_menu/start.visible = false
+		$button_menu/start_again.visible = true
 		visible = !visible
 		pause_game(visible)
 
@@ -19,3 +22,13 @@ func _on_start_button_down():
 
 func pause_game( value : bool):
 	get_tree().paused = value
+	if game_controller.game_started:
+		game_controller.content.visible = !value
+
+
+func _on_start_again_button_down():
+	get_tree().reload_current_scene()
+
+
+func _on_credits_button_down():
+	$credits.visible = !$credits.visible
