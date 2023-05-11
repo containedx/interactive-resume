845cf45ffd4b089217b905cfff6ea4c824298518
Kinga <niu.ula.a@gmail.com>
2023-05-11 13:33:52 +0200

    portfolio fill

--- scenes/UI/project_item.gd
+++ scenes/UI/project_item.gd
@@ -1,21 +1,24 @@
 extends Control
 
-@onready var video_player = $VideoStreamPlayer
+@onready var source = $source
 @onready var button = $Button
 
+@export var video : bool = false
+
 func _process(delta):
-	loop_video()
+	if true:
+		loop_video()
 	reveal_description()
 
 
 func loop_video():
-	if !video_player.is_playing():
-		video_player.play()
+	if !source.is_playing():
+		source.play()
 
 
 func reveal_description():
 	if button.is_hovered():
 		print("hovered")
-		video_player.visible = false
+		source.visible = false
 	else :
-		video_player.visible = true
+		source.visible = true
--- scenes/game_controller.gd
+++ scenes/game_controller.gd
@@ -3,6 +3,8 @@ extends Node2D
 @onready var anim_player = $AnimationPlayer
 @onready var content = $environment/content
 @onready var menu = $cat/Camera2D/menu
+@onready var audio_player = $AudioStreamPlayer
+@onready var cat = $cat
 
 var game_started : bool = false
 
@@ -10,6 +12,21 @@ func _ready():
 	content.visible = false
 	menu.visible = true
 
+func _process(delta):
+	loop_audio()
+	play_if_is_on_porfolio_level()
+
+func loop_audio():
+	if !audio_player.playing:
+		audio_player.play()
+
 func start():
 	anim_player.play("start")
 	game_started = true
+
+func play_if_is_on_porfolio_level():
+	var cat_y = cat.get_global_position().y
+	if  cat_y > 3500 and cat_y < 4000:
+		for item in $environment/content/Portfolio/projects_grid.get_children():
+			item.video = true
+
